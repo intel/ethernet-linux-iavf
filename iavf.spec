6738993ac67321b1991b433d56566b4969210e16
Aleksandr Loktionov <aleksandr.loktionov@intel.com>
2024-12-06 15:36:51 +0100

    Intel official release 4.12.6

--- iavf.spec
+++ iavf.spec
@@ -1,6 +1,6 @@
 Name: iavf
 Summary: Intel(R) Ethernet Adaptive Virtual Function Driver
-Version: 4.12.5
+Version: 4.12.6
 Release: 1
 Source: %{name}-%{version}.tar.gz
 Vendor: Intel Corporation
--- src/iavf_main.c
+++ src/iavf_main.c
@@ -24,8 +24,8 @@ static const char iavf_driver_string[] =
 
 #define DRV_VERSION_MAJOR (4)
 #define DRV_VERSION_MINOR (12)
-#define DRV_VERSION_BUILD (5)
-#define DRV_VERSION "4.12.5"
+#define DRV_VERSION_BUILD (6)
+#define DRV_VERSION "4.12.6"
 const char iavf_driver_version[] = DRV_VERSION;
 static const char iavf_copyright[] =
 	"Copyright (C) 2013-2024 Intel Corporation";
